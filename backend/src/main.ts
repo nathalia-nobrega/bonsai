cdb059a5f1ea89f490c561231fb881818abb6070
Nathalia Nóbrega <ttnast05@gmail.com>
2025-11-07 17:36:14 -0400

    setting up swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* Init Swagger */
+  const config = new DocumentBuilder()
+  .setTitle('Bonsai API v1')
+  .setDescription("The official documentation for the Bonsai API")
+  .setVersion('1.0')
+  .addTag("bonsai")
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
