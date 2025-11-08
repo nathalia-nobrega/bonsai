801f58bc251b2146338d2f6e67ffa920761f7243
Nathalia Nóbrega <ttnast05@gmail.com>
2025-11-07 20:58:41 -0400

    encapsulation, type safety and initialization in user class

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -2,21 +2,29 @@ import { randomUUID } from "crypto";
 
 /* jessica isso aqui ta errado hahahah tem q fazer o encapsulamento direitinho */
 export class User {
+  readonly id: string;
+  readonly createdAt: Date;
+  readonly name: string;
+  readonly email: string;
+  readonly photoUrl: string;
+  readonly level: number;
+  readonly points: number;
 
-    constructor(name, email, photoUrl) {
-      this.id = randomUUID();
-      this.createdAt = new Date()
-      this.photoUrl = photoUrl
-      this.name = name;
-      this.email = email;
-    }
-  
-  id: string
-  createdAt: Date
-  name: string;
-  email: string
-  photoUrl: string
-  level: Number
-  points: Number
-  // password: string
+  constructor(params: {
+    name: string;
+    email: string;
+    photoUrl: string;
+    level?: number;
+    points?: number;
+    id?: string;
+    createdAt?: Date;
+  }) {
+    this.id = params.id ?? randomUUID();
+    this.createdAt = params.createdAt ?? new Date();
+    this.name = params.name;
+    this.email = params.email;
+    this.photoUrl = params.photoUrl;
+    this.level = params.level ?? 1;
+    this.points = params.points ?? 0;
+  }
 }
