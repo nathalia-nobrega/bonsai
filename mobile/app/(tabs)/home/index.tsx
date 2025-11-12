9c356d5adc92f96eec5235f7e4eb9073d1e66755
rebeccajh-dev <rerdr.snf24@uea.edu.br>
2025-11-11 22:25:08 -0400

    Starting home screen

--- mobile/app/(tabs)/home/index.tsx
+++ mobile/app/(tabs)/home/index.tsx
@@ -1,27 +1,36 @@
-// app/(tabs)/home/index.tsx
+//-- this page will answer to home with daily missions, your garden and current mission
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { 
+  View, 
+  Text, 
+  Image, 
+  ImageBackground, 
+  TouchableOpacity, 
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient"; // ✅ import correto
+import { useRouter } from "expo-router";
+import { s } from "./styleHome"
 
-export default function HomeScreen() {
+export default function Index() {
+  const router = useRouter();
+  const {width, height} = useWindowDimensions();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <Text>Bem-vindo à sua jardim!</Text>
-    </View>
-  );
-}
+    <ImageBackground
+      source={require("../../../assets/images/image.png")}
+      style={s.background}
+      resizeMode="cover"
+      >
+      <LinearGradient
+        colors={["rgba(0,0,0,0.1)", "rgba(33, 57, 35, 1)"]}
+        start={{ x: 0.5, y: 0.1 }}
+        end={{ x: 0.5, y: 1 }}
+        style={s.overlay}
+    />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#5C9F60",
-    marginBottom: 10,
-  },
-});
+      <View style={[s.halfWhiteBackground, { paddingTop: height * 0.1, paddingVertical: height * 0.1 } ]}></View>
+      </ImageBackground>
+
+  );
+};
---
+++ mobile/app/(tabs)/home/styleHome.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+ export const s = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 1,
+  },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 16,
+    zIndex: 2,
+  },
+  imageWrapper: {
+    position: "absolute",
+    top: -100,
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  halfWhiteBackground: {
+    width: "100%",
+    height: "60%",
+    backgroundColor: "white",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    paddingHorizontal: 70,
+    paddingVertical: 250,
+    zIndex: 1,
+  },
+});
